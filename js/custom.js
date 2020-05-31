811eec5f3c909b784149b3f1170ae971dd607b5e
jayaprakash <jprakashrpm@gmail.com>
2020-05-31 10:13:29 +0530

    UI updated

--- js/custom.js
+++ js/custom.js
@@ -61,7 +61,7 @@ $(function() {
 
     // typejs
     $('.header .caption h3').typed({
-        strings: ["Web Developer", "UI/UX Designer", "Blogger"],
+        strings: ["Web Developer", "UI Developer", "Blogger"],
         loop: true,
         startDelay: 1000,
         backDelay: 1000,
